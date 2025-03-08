b9ecd8d71e29758293f6269dbe911d6148bfb4ed
TomHoughtonAlliss <tomhoughton18@gmail.com>
2025-03-08 14:03:39 +0000

    Added code to run main

--- main.go
+++ main.go
@@ -1,5 +1,28 @@
 package main
 
-func main() {
+import (
+	"fmt"
 
+	"github.com/countdown/numbers"
+)
+
+func main() {
+	errString := "ERROR: %v\n"
+	var done bool = false
+
+	i := "LSSSSS"
+
+	p, err := numbers.NewPuzzle(i)
+	if err != nil {
+		done = true
+		fmt.Printf(errString, fmt.Errorf("failed to create puzzle: %w", err))
+	}
+
+	for !done {
+		p.Print()
+		if err != nil {
+			done = true
+			fmt.Printf(errString, err.Error())
+		}
+	}
 }

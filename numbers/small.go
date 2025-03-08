5d5d4b2893a5c75ce4572a71077977c3b4581669
TomHoughtonAlliss <tomhoughton18@gmail.com>
2025-03-08 13:47:28 +0000

    Used closure behaviour to match large.go

--- numbers/small.go
+++ numbers/small.go
@@ -4,7 +4,10 @@ import "math/rand"
 
 var smallNums []int = []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
 
-func getSmall() int {
-	i := rand.Intn(len(smallNums))
-	return smallNums[i]
+// newSmallGetter creates a closure that retrieves a random value from the smallNums array.
+func newSmallGetter() getter {
+	return func() (int, error) {
+		i := rand.Intn(len(smallNums))
+		return smallNums[i], nil
+	}
 }

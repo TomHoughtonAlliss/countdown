41e56f7aa1c16a80813e9c18dc49332f83a27cdf
TomHoughtonAlliss <tomhoughton18@gmail.com>
2025-03-08 22:07:19 +0000

    GetPuzzle controller added

--- backend/controllers/puzzle-controller.go
+++ backend/controllers/puzzle-controller.go
@@ -1,7 +1,39 @@
 package controllers
 
-import "github.com/labstack/echo/v4"
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/countdown/number-puzzle/numbers"
+	"github.com/labstack/echo/v4"
+)
 
 func GetPuzzle(c echo.Context) error {
-	return c.JSON(200, "Hello Word!")
+	type puzzleRequest struct {
+		Choices string `json:"choices"`
+		Lower   int    `json:"lowerBound"`
+		Upper   int    `json:"upperBound"`
+	}
+
+	var config puzzleRequest
+	err := c.Bind(&config)
+	if err != nil {
+		return c.String(http.StatusBadRequest, "bad request")
+	}
+
+	p, err := numbers.NewPuzzle(numbers.NewConfig(
+		config.Choices,
+		config.Lower,
+		config.Upper,
+	))
+	if err != nil {
+		return fmt.Errorf("failed to create puzzle: %w", err)
+	}
+
+	res := map[string]any{
+		"target":  p.Target,
+		"numbers": p.Numbers,
+	}
+
+	return c.JSON(200, res)
 }

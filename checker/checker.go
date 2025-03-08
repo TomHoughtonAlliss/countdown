cfa57b48409be8e9472b2d6976c716c8edfc00bb
TomHoughtonAlliss <tomhoughton18@gmail.com>
2025-03-08 20:05:12 +0000

    Commented, formatted, added better syntax handling

--- checker/checker.go
+++ checker/checker.go
@@ -24,6 +24,7 @@ type Checker struct {
 	target int
 }
 
+// NewChecker creates and returns a new Checker object.
 func NewChecker(n []int, t int) Checker {
 	new := make([]int, len(n))
 	copy(new, n)
@@ -35,20 +36,26 @@ func NewChecker(n []int, t int) Checker {
 	}
 }
 
+// Checker.Print gives the current puzzle instance, with up-to-date usable numbers.
 func (c *Checker) Print() {
 	fmt.Printf("Target:\t\t%v\n", c.target)
 	fmt.Printf("Available:\t%v\n", helpers.CommaSeparate(c.current))
 }
 
-func (c *Checker) Expression(expr string) (solved bool, err error) {
+// Checker.Expression takes a string (e.g. 5 + 6) and handles it.
+//
+// The result is calculated and checked against the target.
+//
+// It handles removing used values from our usable numbers array, as well as adding the result.
+func (c *Checker) Expression(expr string) (solved bool, exprErr error, err error) {
 	e, err := NewExpression(expr)
 	if err != nil {
-		return false, fmt.Errorf("failed to create expression: %w", err)
+		return false, fmt.Errorf("failed to create expression: %w", err), nil
 	}
 
 	res, err := e.compute()
 	if err != nil {
-		return false, fmt.Errorf("failed to compute: %w", err)
+		return false, nil, fmt.Errorf("failed to compute: %w", err)
 	}
 
 	e.result = res
@@ -56,22 +63,22 @@ func (c *Checker) Expression(expr string) (solved bool, err error) {
 	c.expressions = append(c.expressions, e)
 
 	if res == c.target {
-		return true, nil
+		return true, nil, nil
 	}
 
 	c.current, err = helpers.Remove(c.current, e.first)
 	if err != nil {
-		return false, fmt.Errorf("failed to remove element: %w", err)
+		return false, nil, fmt.Errorf("failed to remove element: %w", err)
 	}
 
 	c.current, err = helpers.Remove(c.current, e.second)
 	if err != nil {
-		return false, fmt.Errorf("failed to remove element: %w", err)
+		return false, nil, fmt.Errorf("failed to remove element: %w", err)
 	}
 
 	c.current = append(c.current, res)
 
-	return false, nil
+	return false, nil, nil
 }
 
 // Checker.Reset sets all values to their inital configuration.
@@ -84,7 +91,7 @@ func (c *Checker) Reset() {
 	c.expressions = []expression{}
 }
 
-// ToString recurs on our expressions to fully expand our target into the initial values, in the structure given by the user.
+// Checker.ToString recurs on our expressions to fully expand our target into the initial values, in the structure given by the user.
 func (c *Checker) ToString() string {
 	var recur func(int) string
 	recur = func(n int) string {
@@ -110,32 +117,52 @@ func (c *Checker) ToString() string {
 	return equation[1 : len(equation)-1]
 }
 
+// Checker.HandleInput takes a user-inputted string and handles it accordingly.
 func (c *Checker) HandleInput(input string) (done bool, err error) {
 	// TODO break this down more
 	switch input {
-	case "reset":
+	case "reset", "restart":
 		c.Reset()
-	case "next":
+
+	case "next", "skip", "pass":
 		fmt.Print("\nSkipping\n\n")
 		done = true
-	case "quit":
+
+	case "quit", "exit":
 		done = true
 		err = errors.New("exited by user")
-	default:
-		solved, err := c.Expression(input)
-		if err != nil {
-			return true, fmt.Errorf("failed to parse expression: %w", err)
-		}
 
-		done = c.HandleSolve(solved)
+	default:
+		done, err = c.NewExpression(input)
+		if err != nil {
+			return
+		}
 	}
 
 	return done, err
 }
 
-func (c *Checker) HandleSolve(solved bool) bool {
+// Checker.NewExpression handles the parsing and creation of a new user expression.
+func (c *Checker) NewExpression(input string) (bool, error) {
+	solved, exprErr, err := c.Expression(input)
+	if err != nil {
+		return true, fmt.Errorf("failed to parse expression: %w", err)
+	}
+
+	if exprErr != nil {
+		// User inputted a bad command or expression. Continue.
+		fmt.Println()
+		fmt.Println("Unrecognised or unparsable input.")
+		return false, nil
+	}
+
+	return c.HandleSolve(solved), nil
+}
+
+// Checker.HandleSolve checks if the target value has been reached, and if so, returns the final nested equation.
+func (c *Checker) HandleSolve(solved bool) (resolved bool) {
 	if !solved {
-		return false
+		return
 	}
 
 	fmt.Println()

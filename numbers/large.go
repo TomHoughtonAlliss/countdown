eea51e60f831d07697463f358790a2e860601364
TomHoughtonAlliss <tomhoughton18@gmail.com>
2025-03-08 13:46:32 +0000

    Tidied up large number getter

--- numbers/large.go
+++ numbers/large.go
@@ -1,28 +1,30 @@
 package numbers
 
-import "math/rand"
-
-type getter func() int
+import (
+	"errors"
+	"math/rand"
+	"slices"
+)
 
 var largeNums []int = []int{25, 50, 75, 100}
 
+// newLargeGetter creates a closure that retrieves a random value from the largeNums array, and deletes it.
+//
+// The closure will error if called more than four times.
 func newLargeGetter() getter {
 	n := make([]int, len(largeNums))
 	copy(n, largeNums)
 
-	return func() int {
+	var g getter = func() (int, error) {
 		if len(n) == 0 {
-			panic("large number set empty")
+			return 0, errors.New("large number set empty")
 		}
 
 		i := rand.Intn(len(n))
 		num := n[i]
-		n = append(n[:i], n[i+1:]...)
-		return num
+		n = slices.Delete(n, i, i)
+		return num, nil
 	}
 
-}
-
-func getLarge() int {
-	return 0
+	return g
 }
